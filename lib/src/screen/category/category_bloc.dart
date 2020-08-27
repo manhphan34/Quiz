d0f772cfb58ea80f816362dafb04c049476d15b9
Phan Manh <phan.van.manh@sun-asterisk.com>
2020-08-27 12:57:02 +0700

    category

--- lib/src/screen/category/category_bloc.dart
+++ lib/src/screen/category/category_bloc.dart
@@ -6,6 +6,8 @@ import 'package:quiz/src/screen/category/category_state.dart';
 class CategoryBloc extends Bloc<CategoryEvent, CategorySate> {
   var db = AppDatabase.getInstance().modesDao;
   var cats = List<CategoryData>();
+  var quantityQuiz;
+  var difficulty;
 
   CategoryBloc() : super(CategoryInitState());
 
@@ -14,6 +16,10 @@ class CategoryBloc extends Bloc<CategoryEvent, CategorySate> {
     if (event is CategoryLoadingEvent) {
       await db.getCategories.then((value) => cats = value);
       yield CategoryLoadedState(cats: cats);
+    } else if (event is CategorySelectQuizNumberEvent) {
+      yield CategorySelectedQuizNumberState(quantity: event.quantity);
+    } else if (event is CategorySelectQuizDifficultyEvent) {
+      yield CategorySelectedQuizDifficultyState(difficulty: event.difficulty);
     }
   }
 }
--- lib/src/screen/category/category_event.dart
+++ lib/src/screen/category/category_event.dart
@@ -1,9 +1,41 @@
 import 'package:equatable/equatable.dart';
-class CategoryEvent extends Equatable{
+
+class CategoryEvent extends Equatable {
   @override
-  List<Object> get props =>[];
+  List<Object> get props => [];
 }
 
 class CategoryLoadingEvent extends CategoryEvent {}
 
 class CategoryLoadedEvent extends CategoryEvent {}
+
+class CategorySelectQuizNumberEvent extends CategoryEvent {
+  final int quantity;
+
+  CategorySelectQuizNumberEvent({this.quantity});
+
+  @override
+  bool operator ==(Object other) {
+    // TODO: implement ==
+    return quantity == other;
+  }
+
+  @override
+  // TODO: implement hashCode
+  int get hashCode => super.hashCode;
+}
+
+class CategorySelectQuizDifficultyEvent extends CategoryEvent {
+  final String difficulty;
+
+  CategorySelectQuizDifficultyEvent({this.difficulty});
+
+  @override
+  bool operator ==(Object other) {
+    return difficulty == other;
+  }
+
+  @override
+  int get hashCode => super.hashCode;
+
+}
